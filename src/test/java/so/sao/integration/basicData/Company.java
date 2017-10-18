1d39203baf603f127df75a1b7261e77708b7c96c
Leojiajun <984284896@qq.com>
2017-10-18 11:21:25 +0800

    update 1018

--- src/test/java/so/sao/integration/basicData/Company.java
+++ src/test/java/so/sao/integration/basicData/Company.java
@@ -32,13 +32,13 @@ public class Company extends BaseTest{
 		Tools.input("terminalnum", "company", "2", driver);
 		((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)"); //下拉到页面底部
 		Thread.sleep(2000);
-		Tools.button("cardpicture", "company", driver);
-		Thread.sleep(2000);
-		Runtime.getRuntime().exec("D:/uploadexe/companypicture.exe");
-		Thread.sleep(20000);
+//		Tools.button("cardpicture", "company", driver);
+//		Thread.sleep(2000);
+//		Runtime.getRuntime().exec("D:/uploadexe/companypicture.exe");
+//		Thread.sleep(20000);
 		Tools.button("keepcompany", "company", driver);
 		Thread.sleep(1000);
-		Assert.assertTrue(driver.getPageSource().contains("保存成功"));
+		Assert.assertTrue(driver.getPageSource().contains("成功"));
 	}	
 	@Test(priority=3)//审核启用企业
 	public void reviewAndawakenCompany() throws SQLException, InterruptedException{	
